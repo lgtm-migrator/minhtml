623457712daa5b2520696a5462fd07d3efb1093d
R4356th <R4356th@users.noreply.github.com>
2022-08-19 14:33:29 +0000

    chore: reflect automatic changes

--- dist/htmlminifier.esm.bundle.js
+++ dist/htmlminifier.esm.bundle.js
@@ -26646,10 +26646,10 @@ function requireAcorn () {
 		  var defaultOptions = {
 		    // `ecmaVersion` indicates the ECMAScript version to parse. Must be
 		    // either 3, 5, 6 (or 2015), 7 (2016), 8 (2017), 9 (2018), 10
-		    // (2019), 11 (2020), 12 (2021), 13 (2022), or `"latest"` (the
-		    // latest version the library supports). This influences support
-		    // for strict mode, the set of reserved words, and support for
-		    // new syntax features.
+		    // (2019), 11 (2020), 12 (2021), 13 (2022), 14 (2023), or `"latest"`
+		    // (the latest version the library supports). This influences
+		    // support for strict mode, the set of reserved words, and support
+		    // for new syntax features.
 		    ecmaVersion: null,
 		    // `sourceType` indicates the mode the code should be parsed in.
 		    // Can be either `"script"` or `"module"`. This influences global
@@ -26683,8 +26683,9 @@ function requireAcorn () {
 		    // When enabled, super identifiers are not constrained to
 		    // appearing in methods and do not raise an error when they appear elsewhere.
 		    allowSuperOutsideMethod: null,
-		    // When enabled, hashbang directive in the beginning of file
-		    // is allowed and treated as a line comment.
+		    // When enabled, hashbang directive in the beginning of file is
+		    // allowed and treated as a line comment. Enabled by default when
+		    // `ecmaVersion` >= 2023.
 		    allowHashBang: false,
 		    // When `locations` is on, `loc` properties holding objects with
 		    // `start` and `end` properties in `{line, column}` form (with
@@ -26758,6 +26759,9 @@ function requireAcorn () {
 
 		    if (options.allowReserved == null)
 		      { options.allowReserved = options.ecmaVersion < 5; }
+
+		    if (opts.allowHashBang == null)
+		      { options.allowHashBang = options.ecmaVersion >= 14; }
 
 		    if (isArray(options.onToken)) {
 		      var tokens = options.onToken;
@@ -27089,7 +27093,7 @@ function requireAcorn () {
 		    if (refDestructuringErrors.trailingComma > -1)
 		      { this.raiseRecoverable(refDestructuringErrors.trailingComma, "Comma is not permitted after the rest element"); }
 		    var parens = isAssign ? refDestructuringErrors.parenthesizedAssign : refDestructuringErrors.parenthesizedBind;
-		    if (parens > -1) { this.raiseRecoverable(parens, "Parenthesized pattern"); }
+		    if (parens > -1) { this.raiseRecoverable(parens, isAssign ? "Assigning to rvalue" : "Parenthesized pattern"); }
 		  };
 
 		  pp$9.checkExpressionErrors = function(refDestructuringErrors, andThrow) {
@@ -28185,6 +28189,7 @@ function requireAcorn () {
 		  };
 		  pp$8.isDirectiveCandidate = function(statement) {
 		    return (
+		      this.options.ecmaVersion >= 5 &&
 		      statement.type === "ExpressionStatement" &&
 		      statement.expression.type === "Literal" &&
 		      typeof statement.expression.value === "string" &&
@@ -28595,7 +28600,8 @@ function requireAcorn () {
 		      { this.exprAllowed = type.beforeExpr; }
 		  };
 
-		  // Used to handle egde case when token context could not be inferred correctly in tokenize phase
+		  // Used to handle egde cases when token context could not be inferred correctly during tokenization phase
+
 		  pp$6.overrideContext = function(tokenCtx) {
 		    if (this.curContext() !== tokenCtx) {
 		      this.context[this.context.length - 1] = tokenCtx;
@@ -29410,15 +29416,6 @@ function requireAcorn () {
 		          this.raise(this.start, "Comma is not permitted after the rest element");
 		        }
 		        return this.finishNode(prop, "RestElement")
-		      }
-		      // To disallow parenthesized identifier via `this.toAssignable()`.
-		      if (this.type === types$1.parenL && refDestructuringErrors) {
-		        if (refDestructuringErrors.parenthesizedAssign < 0) {
-		          refDestructuringErrors.parenthesizedAssign = this.start;
-		        }
-		        if (refDestructuringErrors.parenthesizedBind < 0) {
-		          refDestructuringErrors.parenthesizedBind = this.start;
-		        }
 		      }
 		      // Parse argument.
 		      prop.argument = this.parseMaybeAssign(false, refDestructuringErrors);
@@ -31849,7 +31846,7 @@ function requireAcorn () {
 
 		  // Acorn is a tiny, fast JavaScript parser written in JavaScript.
 
-		  var version = "8.7.1";
+		  var version = "8.8.0";
 
 		  Parser.acorn = {
 		    Parser: Parser,
